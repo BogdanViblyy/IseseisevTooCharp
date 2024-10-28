f410098f0e90ff1a4c4abcf5e209ff6365b23151
BogdanViblyy <153904857+BogdanViblyy@users.noreply.github.com>
2024-10-28 22:25:54 +0200

    Done

--- IseseisevTooCharp/Program.cs
+++ IseseisevTooCharp/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using IseseisevTooCharp;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace IseseisevTooCsharp
 {
@@ -14,7 +16,7 @@ namespace IseseisevTooCsharp
             Console.InputEncoding = System.Text.Encoding.UTF8;
             while (true)
             {
-                Action[] methods = new Action[] { Task1 };
+                Action[] methods = new Action[] { Task1, Task2};
                 Console.WriteLine("Sisestage ülesanne number");
                 string numstr = Console.ReadLine();
                 int number = int.Parse(numstr);
@@ -89,5 +91,118 @@ namespace IseseisevTooCsharp
                 Console.WriteLine();
             }
         }
+        private static void Task2()
+        {
+            Student[] students = new Student[5];
+
+            // Populate the array with sample student data
+            students[0] = new Student
+            {
+                FirstName = "Bogdan",
+                LastName = "Viblyy",
+                Group = "TARpv23",
+                Subjects = new Subject[]
+                {
+                    new Subject { Name = "Matemaatika", Grades = new int[] { 5, 5, 5 } },
+                    new Subject { Name = "Programmeerimine", Grades = new int[] { 2, 2, 2 } },
+                    new Subject { Name = "Keelekirjandus", Grades = new int[] { 5, 5, 5 } },
+                    new Subject { Name = "Inglise keel", Grades = new int[] { 5, 5, 5 } }
+                }
+            };
+
+            students[1] = new Student
+            {
+                FirstName = "Artur",
+                LastName = "Linder",
+                Group = "VLOGpv24",
+                Subjects = new Subject[]
+                {
+                    new Subject { Name = "Matemaatika", Grades = new int[] { 2, 2, 2 } },
+                    new Subject { Name = "Logistika", Grades = new int[] { 2, 2, 2 } },
+                    new Subject { Name = "Keelekirjandus", Grades = new int[] { 2, 2, 2 } },
+                    new Subject { Name = "Inglise keel", Grades = new int[] { 2, 2, 2 } }
+                }
+            };
+
+            students[2] = new Student
+            {
+                FirstName = "Kirill",
+                LastName = "Sats",
+                Group = "TARpv23",
+                Subjects = new Subject[]
+                {
+                    new Subject { Name = "Matemaatika", Grades = new int[] { 5, 4, 5 } },
+                    new Subject { Name = "Programmeerimine", Grades = new int[] { 4, 5, 5 } },
+                    new Subject { Name = "Keelekirjandus", Grades = new int[] { 3, 2, 4 } },
+                    new Subject { Name = "Inglise keel", Grades = new int[] { 5, 4, 3 } }
+                }
+            };
+
+            students[3] = new Student
+            {
+                FirstName = "Vladislav",
+                LastName = "Kudrjashev",
+                Group = "TARpv23",
+                Subjects = new Subject[]
+                {
+                    new Subject { Name = "Matemaatika", Grades = new int[] { 3, 2, 4 } },
+                    new Subject { Name = "Programmeerimine", Grades = new int[] { 2, 3, 4 } },
+                    new Subject { Name = "Keelekirjandus", Grades = new int[] { 4, 3, 5 } },
+                    new Subject { Name = "Inglise keel", Grades = new int[] { 3, 4, 2 } }
+                }
+            };
+
+            students[4] = new Student
+            {
+                FirstName = "Aleksandr",
+                LastName = "Nadvorny",
+                Group = "TARpv23",
+                Subjects = new Subject[]
+                {
+                    new Subject { Name = "Matematika", Grades = new int[] { 2, 2, 2 } },
+                    new Subject { Name = "Programmeerimine", Grades = new int[] { 2, 2, 2 } },
+                    new Subject { Name = "Keelekirjandus", Grades = new int[] { 2, 2, 2 } },
+                    new Subject { Name = "Inglise keel", Grades = new int[] { 2, 2, 2 } }
+                }
+            };
+            List<Student> studentsForExpulsion = new List<Student>();
+
+
+            foreach (var student in students)
+            {
+                int countOfBadSubjects = 0;
+                foreach (var subject in student.Subjects)
+                {
+                    if (subject.Grades.Average() == 2.0)
+                    { 
+                        countOfBadSubjects++; 
+                    }
+                }
+                if (countOfBadSubjects >= 3) 
+                {
+                    studentsForExpulsion.Add(student);
+                }
+                
+            }
+            Console.WriteLine("Väljaarvamisele minevad tudengid: \n");
+            PrintList(studentsForExpulsion);
+
+
+        }
+        static void PrintList(List<Student> list)
+        {
+            foreach (var student in list)
+            {
+                Console.WriteLine($"Eesnimi: {student.FirstName}\tPerenimi: {student.LastName}\tGrupp: {student.Group}\n");
+
+                foreach (var subject in student.Subjects)
+                {
+                    Console.WriteLine($"  Aine: {subject.Name}, Hinned: {string.Join(", ", subject.Grades)}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
     }
 }
---
+++ IseseisevTooCharp/Subject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IseseisevTooCharp
+{
+    internal class Subject
+    {
+        public string Name { get; set; }
+        public int[] Grades { get; set; }
+    }
+}
